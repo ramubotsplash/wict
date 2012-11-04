7f978e6340c1b574ea43fc43fa639f8d1bd275d5
Ramu Pulipati <ramu_pulipati@yahoo.com>
2012-11-03 23:48:30 -0400

    Added config file

---
+++ config/config.js
@@ -0,0 +1,33 @@
+
+//db: 'mongodb://localhost/noobjs_dev',
+module.exports = {
+    development: {
+      db: 'mongodb://wictwebsvc:Rohi1Labs!@ds041387.mongolab.com:41387/wictweb',
+      facebook: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/facebook/callback"
+      },
+      twitter: {
+          clientID: "CONSUMER_KEY"
+        , clientSecret: "CONSUMER_SECRET"
+        , callbackURL: "http://localhost:3000/auth/twitter/callback"
+      },
+      github: {
+          clientID: 'APP_ID'
+        , clientSecret: 'APP_SECRET'
+        , callbackURL: 'http://localhost:3000/auth/github/callback'
+      },
+      google: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/google/callback"
+      }
+    }
+  , test: {
+
+    }
+  , production: {
+
+    }
+}
